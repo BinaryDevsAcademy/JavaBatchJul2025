b7904e35440fe960c0f4f5d1c98b4d753eeeb20e
Dinesh Sripathi <dineshsri718@gmail.com>
2025-08-29 12:24:26 +0530

    completed DCL commands

--- SQL/DQL_My_SQL.sql
+++ SQL/DQL_My_SQL.sql
@@ -90,4 +90,150 @@ SELECT customer_city, COUNT(customer_city) FROM customers GROUP BY customer_city
 SELECT sale_date, AVG(total_amount) FROM sales GROUP BY sale_date;
 
 -- group the dates where there avg price is greater than or equal to 700
-SELECT sale_date, AVG(total_amount) AS total_amount FROM sales HAVING total_amount >= 700
+SELECT sale_date, AVG(total_amount) AS total_amount FROM sales HAVING total_amount >= 700;
+
+
+-- find the no of orders placed by each customer
+SELECT customer_id, COUNT(customer_id) FROM sales GROUP BY customer_id;
+
+-- find the types of payment methods used by customers
+SELECT payment_method FROM sales GROUP BY payment_method;
+
+
+SELECT sale_id, customer_id, payment_method FROM sales GROUP BY sale_id;
+SELECT sale_id, customer_id, payment_method FROM sales;
+
+-- limit - will limit the no of records to be displayed
+-- Syntax : 
+-- SELECT colName FROM tableName WHERE (optional) condition LIMIT no.of Records;
+-- SELECT colName FROM tableName WHERE (optional) condition LIMIT from record, no.of Records;
+
+SELECT * FROM sales LIMIT 2;
+
+-- find the maximum total Amount in sales table
+SELECT MAX(total_amount) FROM sales;
+
+-- sorting table
+-- Ascending order and Decending order
+-- syntax
+-- SELECT colName FROM tableName ORDER BY colName type (ASC/ DESC);
+
+-- arrange the table column total_amount in ascending order
+SELECT * FROM sales ORDER BY total_amount ASC;
+
+-- find the second maximum total Amount in sales table
+SELECT * FROM sales ORDER BY total_amount DESC LIMIT 1,1;
+
+-- find the type of payment methods in sales table and show them in asc order
+
+-- find the most used payment method
+SELECT payment_method, count(payment_method) AS total FROM sales GROUP BY payment_method ORDER BY total DESC LIMIT 3;
+
+-- Subquery  : query with in another query.
+
+-- find the total no of orders placed by customer who are having email rahul.sharma@example.com
+
+SELECT COUNT(sale_id) FROM sales GROUP BY customer_id HAVING customer_id = 
+(SELECT customer_id FROM customers WHERE customer_email = "rahul.sharma@example.com");
+
+-- find the total amount spent by customers who are living in city 3
+
+-- selecting customerId from cutsomer table using customer_city
+-- find sales amount by customerId
+SELECT SUM(total_amount) FROM sales WHERE customer_id IN (SELECT customer_id FROM customers WHERE customer_city = 3);
+
+
+-- find the customer who are living in Salt Lake
+
+SELECT customer_name FROM customers WHERE customer_city = (SELECT location_id FROM location WHERE area_name = "Salt Lake");
+
+-- find how many orders were place from location Bandra West
+SELECT COUNT(sale_id) FROM sales WHERE customer_id IN (
+SELECT customer_id FROM customers WHERE customer_city = (
+SELECT location_id FROM location WHERE area_name = "Bandra West"));
+
+
+-- find the customer name who are having payment status pending
+SELECT customer_name FROM customers WHERE customer_id IN (
+SELECT customer_id FROM sales WHERE status = "pending");
+
+-- JOINs
+-- 3 types
+-- inner join
+-- Syntax : 
+-- SELECT colNames FROM table1 JOIN table2 ON condition (reference columns);
+-- outer join
+-- 1. left outer join
+SELECT * FROM customers c LEFT OUTER JOIN location l ON c.customer_city = l.location_id;
+-- 2. right outer join
+SELECT * FROM customers c RIGHT OUTER JOIN location l ON c.customer_city = l.location_id;
+-- 3. full outer join
+
+-- cross join
+
+SELECT 
+c.customer_id, 
+c.customer_name, 
+c.customer_email, 
+c.gender, 
+c.customer_phone,
+l.area_name,
+l.area_pincode,
+l.city_name,
+l.state_name 
+FROM customers c LEFT OUTER JOIN location l ON c.customer_city = l.location_id;
+
+
+-- views : Views : 
+-- used to create virtual tables in sql.
+
+-- Virtual Tables : 
+--     These are not the physical tables but will act similar to physical one.
+-- Which helps us to perform CRUD operations 
+
+-- -> If we modify the data in views table the data will be modified in the
+-- original table as well
+
+-- syntax : 
+-- CREATE VIEW viewName AS QUERY
+
+
+CREATE VIEW customersDataWithLocation AS SELECT 
+c.customer_id, 
+c.customer_name, 
+c.customer_email, 
+c.gender, 
+c.customer_phone,
+l.area_name,
+l.area_pincode,
+l.city_name,
+l.state_name 
+FROM customers c LEFT OUTER JOIN location l ON c.customer_city = l.location_id;
+
+
+SELECT * FROM customersdatawithlocation;
+
+UPDATE customersdatawithlocation SET area_name = "Gandhi Nagar" WHERE customer_id = 1;
+-- updates the entire view - virtual table
+CREATE OR REPLACE VIEW customersDataWithLocation AS SELECT * FROM customers;
+
+-- delete a view
+DROP VIEW customersDataWithLocation;
+
+
+-- DCL - Data Control Lang
+-- root - admin
+-- database - tables - access - users
+-- DQL, DML, DDL
+-- To find the users in database
+SELECT User, host FROM mysql.user;	
+
+-- creating user
+CREATE USER "dinesh"@"localhost" IDENTIFIED BY "1234";
+-- GRANT - provides the permission to the user
+GRANT SELECT,INSERT ON brollyacademy.sales TO "dinesh"@"localhost";
+
+GRANT ALL PRIVILEGES ON brollyacademy.sales TO 'dinesh'@'localhost';
+
+-- REVOKE - Retrives the permission from the user
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'dinesh'@'localhost';
